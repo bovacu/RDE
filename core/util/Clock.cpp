afe00ccf93dc14c081ce28d0c1481ab0389eff23
Borja <vazquezborja@hotmail.es>
2022-02-04 21:22:06 +0100

    Completely deleted glfw

--- core/util/Clock.cpp
+++ core/util/Clock.cpp
@@ -1,26 +1,28 @@
 #include "Clock.h"
 
-#include <GLFW/glfw3.h>
 #include "Delta.h"
 
 namespace engine {
 
-    Clock::Clock() : startTime((float)glfwGetTime()) {  }
+    Clock::Clock() : startTime(0) {  }
 
     float Clock::getElapsedTimeSc() const {
-        Delta _now = (float)glfwGetTime();
-        return _now - startTime;
+//        Delta _now = (float)glfwGetTime();
+//        return _now - startTime;
+        return 0;
     }
 
     float Clock::getElapsedTimeMs() const {
-        Delta _now = (float)glfwGetTime();
-        return (_now - startTime) * 1000;
+//        Delta _now = (float)glfwGetTime();
+//        return (_now - startTime) * 1000;
+        return 0;
     }
 
     float Clock::restart() {
-        Delta _now = (float)glfwGetTime();
-        float _elapsed = _now - startTime;
-        startTime = _now;
-        return _elapsed;
+//        Delta _now = (float)glfwGetTime();
+//        float _elapsed = _now - startTime;
+//        startTime = _now;
+//        return _elapsed;
+        return 0;
     }
 }
