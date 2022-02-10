2231bc09ae63ebe4ee5f8feae67ef2ad3c749ec0
Borja <vazquezborja@hotmail.es>
2022-02-10 20:02:37 +0100

    Improvements
    - Now undesired events are ignored, it is customizable
    - Mobile basic input created
    - Fixed some small problems with other inputs

---
+++ engine/include/core/render/window/event/MobileEvent.h
@@ -0,0 +1,7 @@
+// Created by borja on 10/2/22.
+
+
+#ifndef ENGINE2_0_MOBILEEVENT_H
+#define ENGINE2_0_MOBILEEVENT_H
+
+#endif //ENGINE2_0_MOBILEEVENT_H
---
+++ engine/include/core/render/window/input/MobileInput.h
@@ -0,0 +1,7 @@
+// Created by borja on 10/2/22.
+
+
+#ifndef ENGINE2_0_MOBILEINPUT_H
+#define ENGINE2_0_MOBILEINPUT_H
+
+#endif //ENGINE2_0_MOBILEINPUT_H
