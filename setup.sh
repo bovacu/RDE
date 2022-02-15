6927041184d5817313e072c61779355f1d4edccb
Borja <vazquezborja@hotmail.es>
2022-02-15 23:26:46 +0100

    Improved setup

--- setup.sh
+++ setup.sh
@@ -4,17 +4,16 @@ sudo apt-get install clang
 
 cmake -Blibs/SDL/build -Slibs/SDL
 cd libs/SDL/build && make
-cd libs/SDL/build && make install
+sudo make install
 
-cmake -Blibs/SDL_image/build -Slibs/SDL_image
-cd libs/SDL_image/build && make
-cd libs/SDL_image/build && make install
-
-cmake -Blibs/SDL_net/build -Slibs/SDL_net
-cd libs/SDL_net/build && make
-cd libs/SDL_net/build && make install
-
+cd ../../..
 cmake -Blibs/SDL_mixer/build -Slibs/SDL_mixer
 cd libs/SDL_mixer/build && make
-cd libs/SDL_mixer/build && make install
+sudo make install
+
+cd ../../..
+cmake -Blibs/SDL_image/build -Slibs/SDL_image
+echo "$PWD"
+cd libs/SDL_image/build && make
+sudo make install
 
