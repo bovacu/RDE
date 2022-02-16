f2193060967422a725d8032d221969aa1db04a82
Borja <vazquezborja@hotmail.es>
2022-02-16 14:41:31 +0100

    More setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
-sudo apt-get install build-essential cmake clang git autoconf
+sudo apt-get install build-essential cmake clang git autoconf libtool m4 automake
 git submodule init
 git submodule update
 cd submodules/imgui && git checkout docking
-cd ../..
 
-sed -i -e 's/add_library(jpeg STATIC)/add_library(jpeg SHARED)/g' submodules/SDL_image/external/jpeg-9d/CMakeLists.txt
+cd ../.. && sed -i -e 's/add_library(jpeg STATIC)/add_library(jpeg SHARED)/g' submodules/SDL_image/external/jpeg-9d/CMakeLists.txt
 
 cd submodules/SDL && ./autogen && ./configure && make && sudo make install && cd ../..
 cd submodules/SDL_image && ./autogen && ./configure && make && sudo make install && cd ../..
