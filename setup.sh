dd55764857b41af74384f0e89da36969eea18ef1
Borja <vazquezborja@hotmail.es>
2022-02-16 03:03:04 +0100

    Hopefully last decoupling in Linux

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 sudo apt-get install build-essential
 sudo apt-get install cmake
 sudo apt-get install clang
+sudo apt-get install git
 
-cmake -Blibs/SDL/build -Slibs/SDL
+cmake -Blibs/SDL/build -Slibs/SDL -DBUILD_SHARED_LIBS=FALSE
 cd libs/SDL/build && make
 sudo make install
 
 cd ../../..
-cmake -Blibs/SDL_mixer/build -Slibs/SDL_mixer
-cd libs/SDL_mixer/build && make
+cd libs/SDL_mixer && ./configure
+make
 sudo make install
 
 cd ../../..
-cmake -Blibs/SDL_image/build -Slibs/SDL_image
-echo "$PWD"
+cmake -Blibs/SDL_image/build -Slibs/SDL_image -DBUILD_SHARED_LIBS=FALSE
 cd libs/SDL_image/build && make
 sudo make install
 
