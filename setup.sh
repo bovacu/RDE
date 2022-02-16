7e18d7dcb7fc0f4dd6278b664ef2eaa4b24c3ea1
Borja <vazquezborja@hotmail.es>
2022-02-16 14:03:11 +0100

    Almost finished setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,14 @@
-sudo apt-get install build-essential
-sudo apt-get install cmake
-sudo apt-get install clang
-sudo apt-get install git
+sudo apt-get install build-essential cmake clang git autoconf
+git submodule init
+git submodule update
+cd submodules/imgui && git checkout docking
+cd ..
 
-cmake -Blibs/SDL/build -Slibs/SDL -DBUILD_SHARED_LIBS=FALSE
-cd libs/SDL/build && make
-sudo make install
+sed -i -e 's/add_library(jpeg STATIC)/add_library(jpeg SHARED)/g' submodules/SDL_image/external/jpeg-9d/CMakeLists.txt
 
-cd ../../..
-cd libs/SDL_mixer && ./configure
-make
-sudo make install
-
-cd ../../..
-cmake -Blibs/SDL_image/build -Slibs/SDL_image -DBUILD_SHARED_LIBS=FALSE
-cd libs/SDL_image/build && make
-sudo make install
+cd submodules/SDL && ./configure && make && sudo make install && cd ..
+cd submodules/SDL_image && ./configure && make && sudo make install && cd ..
+cd submodules/SDL_mixer && ./configure && make && sudo make install && cd ..
+cd submodules/SDL_net && ./configure && make && sudo make install && cd ..
+cd submodules/freetype && ./autogen && ./configure && make && sudo make install && cd ..
 
