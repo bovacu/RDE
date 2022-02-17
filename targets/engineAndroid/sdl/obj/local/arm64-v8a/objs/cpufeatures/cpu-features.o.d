35c07e79eb8fa9c96147e6c58d4eeb64d4796c59
Borja <vazquezborja@hotmail.es>
2022-02-17 13:54:05 +0100

    removed old android

--- targets/engineAndroid/sdl/obj/local/arm64-v8a/objs/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/borja/Documents/C++/Engine2.0/targets/engineAndroid/sdl/obj/local/arm64-v8a/objs/cpufeatures/cpu-features.o: \
-  /home/borja/Android/Sdk/ndk/20.1.5948944/sources/android/cpufeatures/cpu-features.c \
-  /home/borja/Android/Sdk/ndk/20.1.5948944/sources/android/cpufeatures/cpu-features.h
-
-/home/borja/Android/Sdk/ndk/20.1.5948944/sources/android/cpufeatures/cpu-features.h:
