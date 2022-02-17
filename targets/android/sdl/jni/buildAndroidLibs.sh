b3848df0349d577ddcceb33b835518bd2393f3c9
Borja <vazquezborja@hotmail.es>
2022-02-17 14:01:53 +0100

    Removed android target to make it a separated module

--- targets/android/sdl/jni/buildAndroidLibs.sh
+++
@@ -1,2 +0,0 @@
-/home/borja/Android/Sdk/ndk/20.1.5948944/ndk-build APP_PLATFORM=android-21 NDK_DEBUG=1 NDK_OUT=/home/borja/Documents/C++/Engine2.0/targets/android/sdl/build/intermediates/ndkBuild/debug/object NDK_LIBS_OUT=/home/borja/Documents/C++/Engine2.0/targets/android/sdl/build/intermediates/ndkBuild/debug/lib
-
