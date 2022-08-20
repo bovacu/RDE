7d818b63e75495c9df7f4fe569198fc8f0d345e1
Borja Vazquez <borja.vazquez@wildlifestudios.com>
2022-08-20 20:46:32 -0300

    Imgui in Editor only for desktop

--- editor/Editor.cpp
+++ editor/Editor.cpp
@@ -5,20 +5,25 @@
 #include "Editor.h"
 #include "core/graph/Components.h"
 #include "core/graph/Scene.h"
+
+#if !IS_MOBILE()
 #include "imgui.h"
+#endif
 
 namespace Editor {
 
     void Editor::redirectRendering(FrameBuffer* _frameBuffer) {
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
-        ImGui::Begin("Scene", nullptr, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
-        ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-        auto viewportSize = ImVec2 { viewportPanelSize.x, viewportPanelSize.y };
+        #if !IS_MOBILE()
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
+            ImGui::Begin("Scene", nullptr, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
+            ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
+            auto viewportSize = ImVec2 { viewportPanelSize.x, viewportPanelSize.y };
 
-        uint32_t textureID = _frameBuffer->getColorAttachmentRendererID();
-        ImGui::Image((void*)(intptr_t)textureID, ImVec2{ 1280, 720 }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
-        ImGui::End();
-        ImGui::PopStyleVar();
+            uint32_t textureID = _frameBuffer->getColorAttachmentRendererID();
+            ImGui::Image((void*)(intptr_t)textureID, ImVec2{ 1280, 720 }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
+            ImGui::End();
+            ImGui::PopStyleVar();
+        #endif
     }
 
     void Editor::onInit() {
