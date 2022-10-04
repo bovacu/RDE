ef0be5a19acfad4e30f8b0e436562bfba4fd9fc5
Borja Vazquez <borja.vazquez@wildlifestudios.com>
2022-10-04 14:17:29 -0300

    Updated setup and CMake for Apple

--- vcpkgSetupMac.sh
+++ vcpkgSetupMac.sh
@@ -12,7 +12,7 @@ vcpkg/vcpkg install freetype[core]:x64-osx
 vcpkg/vcpkg install sdl2[core,x11]:x64-osx
 vcpkg/vcpkg install sdl2-image[core,libjpeg-turbo]:x64-osx
 vcpkg/vcpkg install sdl2-mixer[mpg123,libvorbis]:x64-osx
-# vcpkg/vcpkg install glad:x64-osx
+vcpkg/vcpkg install glad:x64-osx
 vcpkg/vcpkg install imgui[docking-experimental,opengl3-binding,sdl2-binding]:x64-osx
 vcpkg/vcpkg install glm:x64-osx
 vcpkg/vcpkg install nlohmann-json:x64-osx
