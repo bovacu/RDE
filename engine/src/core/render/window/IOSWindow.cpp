9766ac0fb9c983ca1a32e6efd167f9321626c66b
Borja Vazquez Cuesta <vazquezborja@hotmail.es>
2023-02-16 01:04:27 -0300

    Fix iOSWindow configuration

--- engine/src/core/render/window/IOSWindow.cpp
+++ engine/src/core/render/window/IOSWindow.cpp
@@ -14,15 +14,16 @@ namespace RDE {
         Util::Log::debug("Platform: IOS");
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,3);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
-        SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-        SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-        SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-        SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
 
         SDL_DisplayMode displayMode;
-        SDL_GetDesktopDisplayMode(0, &displayMode);
-        window = SDL_CreateWindow(nullptr, 0, 0, displayMode.w, displayMode.h,  SDL_WINDOW_OPENGL);
+        SDL_GetDisplayMode(0, 0, &displayMode);
+        window = SDL_CreateWindow(nullptr, 0, 0, displayMode.w, displayMode.h,  SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+        
+        int drawableSizeX = 0, drawableSizeY = 0;
+        SDL_GL_GetDrawableSize(window, &drawableSizeX, &drawableSizeY);
+        Util::Log::debug("Screen Size: (", displayMode.w, ", ", displayMode.h, "), Drawable Size: (", drawableSizeX, ", ", drawableSizeY, ")");
 
         if(window == nullptr) {
             Util::Log::error("SDL window creation failed: ", SDL_GetError());
