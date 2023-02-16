fd361110be1e222e68d7c2403ad65c3074739378
Borja Vazquez Cuesta <vazquezborja@hotmail.es>
2023-02-16 01:24:00 -0300

    Fixed fragment shader for text on ES

--- defaultAssets/shaders/es/texture/textFragment.glsl
+++ defaultAssets/shaders/es/texture/textFragment.glsl
@@ -9,7 +9,7 @@ uniform sampler2D tex;
 out vec4 out_color;
 
 void main(void) {
-	float d = texture(tex, uv).r;
+	float d = texture(tex, uv).a;
 	float aaf = fwidth(d);
 	float alpha = smoothstep(0.5 - aaf, 0.5 + aaf, d);
 	out_color = vec4(color.rgb, alpha);
