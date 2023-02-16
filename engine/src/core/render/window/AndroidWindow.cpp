1f5e0661e013ddb82fc476e573b3fbb0956dd913
Borja Vazquez Cuesta <vazquezborja@hotmail.es>
2023-02-16 01:03:50 -0300

    Fix some android windows configurations

--- engine/src/core/render/window/AndroidWindow.cpp
+++ engine/src/core/render/window/AndroidWindow.cpp
@@ -20,11 +20,14 @@ namespace RDE {
             int width = mode.w;
             int height = mode.h;
             SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL,1);
-            SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
             SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,24);
-            window = SDL_CreateWindow(nullptr,0,0,width,height,SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_RESIZABLE);
+            window = SDL_CreateWindow(nullptr,0,0,width,height,SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
             SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES );
             SDL_SetHint(SDL_HINT_ORIENTATIONS, "Portrait");
+
+            int drawableSizeX = 0, drawableSizeY = 0;
+            SDL_GL_GetDrawableSize(window, &drawableSizeX, &drawableSizeY);
+            Util::Log::debug("Screen Size: (", width, ", ", height, ")", "Drawable Size: (", drawableSizeX, ", ", drawableSizeY, ")");
 
             if(window == nullptr) {
                 Util::Log::error("SDL window creation failed: ", SDL_GetError());
