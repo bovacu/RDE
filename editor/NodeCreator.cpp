89e677ed07fbbf2fed637308594221b72cb0d599
Bovacu <vazquezborja@hotmail.es>
2023-05-04 15:13:06 -0300

    Added UIText to the node creation tool

--- editor/NodeCreator.cpp
+++ editor/NodeCreator.cpp
@@ -18,6 +18,12 @@ UIPanel* createPanelNode(Editor* _editor) {
 	auto _panelNode = _editor->canvas->graph->createNode(Util::String::appendToString("Panel_", _editor->graph->getNodeContainer().size()));
 	auto* _panel = _panelNode->addComponent<UIPanel>(UIPanelConfig { .size = {256, 256} });
 	_panel->setFramebuffer(_editor->editorData.gameViewFramebufferID | _editor->editorData.sceneViewFramebufferID);
-	Util::Log::info("FB: ", _panel->getFramebuffer());
 	return _panel;
+}
+
+UIText* createUITextNode(Editor* _editor) {
+	auto _uiTextNode = _editor->canvas->graph->createNode(Util::String::appendToString("UIText_", _editor->graph->getNodeContainer().size()));
+	auto* _text = _uiTextNode->addComponent<UIText>(UITextConfig { .font = _editor->engine->manager.fontManager.getFont("MontserratRegular") });
+	_text->setFramebuffer(_editor->editorData.gameViewFramebufferID | _editor->editorData.sceneViewFramebufferID);
+	return _text;
 }
