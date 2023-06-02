d625bb3bcb40398252c662754c68a990aab89476
Borja Vazquez <borja.vazquez@wildlifestudios.com>
2023-06-02 13:50:44 -0300

    use the basic implementation and initialization of ImGui I previously disabled

--- engine/src/core/graph/ImGuiScene.cpp
+++ engine/src/core/graph/ImGuiScene.cpp
@@ -1,25 +1,18 @@
 #include "core/Core.h"
 #if !IS_MOBILE()
 
-// #include "imgui.h"
+#include "imgui_impl_opengl3.h"
 #include "core/graph/ImGuiScene.h"
 
 #include "core/Engine.h"
 #include "core/systems/eventSystem/MouseEvent.h"
-#include "core/systems/console/Console.h"
 #include "core/graph/Graph.h"
-#include "core/render/RenderManager.h"
 #include "core/graph/components/Transform.h"
-#include "core/graph/components/TextRenderer.h"
 #include "core/graph/components/ui/UIButton.h"
 #include "core/graph/components/ui/UIPanel.h"
 #include "core/graph/components/ui/UIText.h"
-#include "core/graph/components/ui/UIAnchoring.h"
-#include "core/graph/components/ui/UIMask.h"
 #include "core/graph/components/ui/UIImage.h"
-#include "core/graph/components/ComponentBase.h"
 #include "core/graph/components/Node.h"
-#include "core/systems/uiSystem/Canvas.h"
 
 namespace RDE {
 
@@ -70,86 +63,86 @@ namespace RDE {
 
     void ImGuiScene::onInit() {
         // Setup Dear ImGui context
-//         #ifndef __EMSCRIPTEN__
-//         IMGUI_CHECKVERSION();
-//         #endif
+         #ifndef __EMSCRIPTEN__
+         IMGUI_CHECKVERSION();
+         #endif
 
-//         i_Context = ImGui::CreateContext();
-// //        p_Context = ImPlot::CreateContext();
-//         ImGuiIO& io = ImGui::GetIO(); (void)io;
-//         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-//         io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-//         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-// //        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-//         //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons;
-//         //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
+         i_Context = ImGui::CreateContext();
+ //        p_Context = ImPlot::CreateContext();
+         ImGuiIO& io = ImGui::GetIO(); (void)io;
+         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+         io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+ //        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+         //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons;
+         //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
 
-//         // Setup Dear ImGui style
-//         ImGui::StyleColorsDark();
+         // Setup Dear ImGui style
+         ImGui::StyleColorsDark();
 
-//         // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
-//         ImGuiStyle& style = ImGui::GetStyle();
-//         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-//             style.WindowRounding = 0.0f;
-//             style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-//         }
+         // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+         ImGuiStyle& style = ImGui::GetStyle();
+         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+             style.WindowRounding = 0.0f;
+             style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+         }
 
-//         auto* window = static_cast<SDL_Window*>(engine->getWindow()->getNativeWindow());
+         auto* window = static_cast<SDL_Window*>(engine->getWindow()->getNativeWindow());
 
-//         // Setup Platform/Renderer bindings
-//         ImGui_ImplSDL2_InitForOpenGL(window, engine->getWindow()->getContext());
+         // Setup Platform/Renderer bindings
+         ImGui_ImplSDL2_InitForOpenGL(window, engine->getWindow()->getContext());
         
-//         #ifdef __EMSCRIPTEN__
-//         ImGui_ImplOpenGL3_Init("#version 300 es");
-//         #else
-//         ImGui_ImplOpenGL3_Init("#version 410");
-//         #endif
+         #ifdef __EMSCRIPTEN__
+         ImGui_ImplOpenGL3_Init("#version 300 es");
+         #else
+         ImGui_ImplOpenGL3_Init("#version 410");
+         #endif
 
-//         for(auto& _state : State::stateToNameDict) {
-//             plotBuffers[_state.first] = {};
-//         }
+         for(auto& _state : State::stateToNameDict) {
+             plotBuffers[_state.first] = {};
+         }
 
-//         mseDel.bind<&ImGuiScene::onMouseScrolled>(this);
-//         mbpeDel.bind<&ImGuiScene::onMouseClicked>(this);
-//         mmeDel.bind<&ImGuiScene::onMouseMovedEvent>(this);
+         mseDel.bind<&ImGuiScene::onMouseScrolled>(this);
+         mbpeDel.bind<&ImGuiScene::onMouseClicked>(this);
+         mmeDel.bind<&ImGuiScene::onMouseMovedEvent>(this);
     }
 
     void ImGuiScene::onEvent(Event& _e) {
-        // EventDispatcher dispatcher(_e);
-        // dispatcher.dispatchEvent<MouseScrolledEvent>(mseDel);
-        // dispatcher.dispatchEvent<MouseButtonPressedEvent>(mbpeDel);
-        // dispatcher.dispatchEvent<MouseMovedEvent>(mmeDel);
+         EventDispatcher dispatcher(_e);
+         dispatcher.dispatchEvent<MouseScrolledEvent>(mseDel);
+         dispatcher.dispatchEvent<MouseButtonPressedEvent>(mbpeDel);
+         dispatcher.dispatchEvent<MouseMovedEvent>(mmeDel);
     }
 
     void ImGuiScene::onEnd() {
-        // ImGui_ImplOpenGL3_Shutdown();
-        // ImGui_ImplSDL2_Shutdown();
-        // ImGui::DestroyContext(i_Context);
+         ImGui_ImplOpenGL3_Shutdown();
+         ImGui_ImplSDL2_Shutdown();
+         ImGui::DestroyContext(i_Context);
     }
 
     void ImGuiScene::begin() {
-        // ImGui_ImplOpenGL3_NewFrame();
-        // ImGui_ImplSDL2_NewFrame();
-        // ImGui::NewFrame();
+         ImGui_ImplOpenGL3_NewFrame();
+         ImGui_ImplSDL2_NewFrame();
+         ImGui::NewFrame();
     }
 
     void ImGuiScene::end() {
-        // ImGuiIO& io = ImGui::GetIO();
-        // io.DisplaySize = ImVec2((float)engine->getWindow()->getWidth(), (float)engine->getWindow()->getHeight());
+         ImGuiIO& io = ImGui::GetIO();
+         io.DisplaySize = ImVec2((float)engine->getWindow()->getWidth(), (float)engine->getWindow()->getHeight());
 
-        // // Rendering
-        // ImGui::Render();
-        // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+         // Rendering
+         ImGui::Render();
+         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        // if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-        //     SDL_Window* backup_current_window = engine->getWindow()->getNativeWindow();
-        //     SDL_GLContext backup_current_context = engine->getWindow()->getContext();
-        //     ImGui::UpdatePlatformWindows();
-        //     ImGui::RenderPlatformWindowsDefault();
-        //     SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
-        // }
+         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+             SDL_Window* backup_current_window = engine->getWindow()->getNativeWindow();
+             SDL_GLContext backup_current_context = engine->getWindow()->getContext();
+             ImGui::UpdatePlatformWindows();
+             ImGui::RenderPlatformWindowsDefault();
+             SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+         }
 
-        // resetID();
+         resetID();
     }
 
     void ImGuiScene::drawDebugInfo(Scene* _scene) {
