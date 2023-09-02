c67f0b4654736582206db1ad3b2c0bbd4b5fef8f
Bovacu <vazquezborja@hotmail.es>
2023-09-02 16:09:59 -0300

    Added warnings as error for compilation

--- build_linux_shared.sh
+++ build_linux_shared.sh
@@ -1,3 +1,3 @@
 cd build
-clang++ ../engine/src/rde.cpp -shared -fPIC -DRDE_AUDIO_MODULE -DRDE_PHYSICS_MODULE -I../engine/include -I../engine/src -I../external -I../external/linux/include -L../external/linux/lib -lm -lSDL2 -lglad -o libRDE.so
+clang++ ../engine/src/rde.cpp -shared -fPIC -DRDE_AUDIO_MODULE -DRDE_PHYSICS_MODULE -I../engine/include -I../engine/src -I../external -I../external/linux/include -L../external/linux/lib -lm -lSDL2 -lglad -Werror -Wall -Wextra -o libRDE.so
 cd ..
