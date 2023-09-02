43b3e6710ee7763127ee8282c47aa66b66ca36b7
Borja vazquez <boja.vazquez@byaliens.com>
2023-09-02 03:33:10 -0300

    Return to original dir after finishing compiling for windows

--- build_windows.bat
+++ build_windows.bat
@@ -3,3 +3,6 @@ cd build
 
 @echo on
 clang++ ..\engine\src\rde.cpp -shared -DRDE_AUDIO_MODULE -DRDE_PHYSICS_MODULE -I../engine/include -I../external -I../external/windows/include -L../external/windows/lib -L../external/windows/lib/manual-link -lSDL2main -lSDL2 -lSDL2_image -lglad -o RDE.dll
+@echo off
+
+cd ..
