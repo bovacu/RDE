2a40bded7f36dd99468b93a36342518822c0dbe2
Bovacu <vazquezborja@hotmail.es>
2023-09-05 22:18:46 -0300

    Added debug symbols to building

--- build_linux_shared.sh
+++ build_linux_shared.sh
@@ -1,3 +1,3 @@
 cd build
-clang++ ../engine/src/rde.cpp -shared -fPIC -DRDE_AUDIO_MODULE -DRDE_PHYSICS_MODULE -I../engine/include -I../engine/src -I../external -I../external/linux/include -L../external/linux/lib -ldl -lm -lpthread -lSDL2 -lglad -Werror -Wall -Wextra -Wno-tautological-constant-out-of-range-compare -o libRDE.so
+clang++ -g -O0 ../engine/src/rde.cpp -shared -fPIC -DRDE_AUDIO_MODULE -DRDE_PHYSICS_MODULE -I../engine/include -I../engine/src -I../external -I../external/linux/include -L../external/linux/lib -ldl -lm -lpthread -lSDL2 -lglad -Werror -Wall -Wextra -Wno-tautological-constant-out-of-range-compare -o libRDE.so
 cd ..
--- build_linux_static.sh
+++ build_linux_static.sh
@@ -1,5 +1,5 @@
 cd build
-clang++ ../engine/src/rde.cpp -DRDE_AUDIO_MODULE -DRDE_PHYSICS_MODULE -I../engine/include -I../engine/src -I../external -I../external/linux/include -ldl -lm -lpthread -Werror -Wall -Wextra -Wno-tautological-constant-out-of-range-compare -o libRDE.o
+clang++ -g -O0 ../engine/src/rde.cpp -DRDE_AUDIO_MODULE -DRDE_PHYSICS_MODULE -I../engine/include -I../engine/src -I../external -I../external/linux/include -ldl -lm -lpthread -Werror -Wall -Wextra -Wno-tautological-constant-out-of-range-compare -o libRDE.o
 ar r libRDE.o libRDE.a
 rm libRDE.o
 cd ..
