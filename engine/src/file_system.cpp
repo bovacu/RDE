f9c8657873d626f631f44af1f09e8bdaf4b3619d
Bovacu <vazquezborja@hotmail.es>
2023-09-10 21:33:56 -0300

    Read files as C style

--- engine/src/file_system.cpp
+++ engine/src/file_system.cpp
@@ -1,15 +1,27 @@
-#include <fstream>
-
 rde_atlas_sub_textures rde_file_system_read_atlas_config(const char* _atlas_path, rde_texture* _atlas) {
-	std::ifstream _stream(_atlas_path);
-	_stream.seekg(0, std::ios::end);
-	size_t _size = _stream.tellg();
-	std::string _buffer(_size, ' ');
-	_stream.seekg(0);
-	_stream.read(&_buffer[0], _size); 
+	FILE* _file = nullptr;
+	char* _text = nullptr;
+
+	#if IS_WINDOWS()
+		errno_t _err = fopen_s(&_file, _atlas_path, "r");
+		assert(_err == 0 && "File could not be opened");
+	#else
+		_file = fopen(_config_rde_path, "r");
+		assert(_file != nullptr && "File could not be opened");
+	#endif
+
+	long _num_bytes = 0;
+	fseek(_file, 0L, SEEK_END);
+	_num_bytes = ftell(_file);
+	fseek(_file, 0L, SEEK_SET);
+
+	_text = (char*)calloc(_num_bytes, sizeof(char));
+	assert(_text != nullptr && "Could not allocate memory for reading the config file");
+	fread(_text, sizeof(char), _num_bytes, _file);
+
 
 	rde_atlas_sub_textures _atlas_sub_textures;
-	cJSON* _atlas_json = cJSON_Parse(_buffer.c_str());
+	cJSON* _atlas_json = cJSON_Parse(_text);
 
 	if(_atlas_json == nullptr) {
 		const char* _error_ptr = cJSON_GetErrorPtr();
@@ -38,6 +50,9 @@ rde_atlas_sub_textures rde_file_system_read_atlas_config(const char* _atlas_path
 	}
 
 	cJSON_free(_atlas_json);
+
+	fclose(_file);
+	free(_text);
 
 	return _atlas_sub_textures;
 }
