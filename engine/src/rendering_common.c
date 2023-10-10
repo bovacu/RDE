35409c228f29b4989583f405bd7d9c04353d3be7
Bovacu <vazquezborja@hotmail.es>
2023-10-10 01:12:39 -0300

    Moved GL check error function

--- engine/src/rendering_common.c
+++ engine/src/rendering_common.c
@@ -21,43 +21,6 @@ rde_vec_2F rde_rendering_get_aspect_ratio() {
 	return _aspect_ratios;
 }
 
-bool rde_util_check_opengl_error(const char* _message) {
-	GLenum _err;
-	while((_err = glGetError()) != GL_NO_ERROR){
-		switch(_err) {
-			case GL_NO_ERROR:
-				printf("GL_NO_ERROR: No error has been recorded. The value of this symbolic constant is guaranteed to be 0. | %s -> %u \n",  _message, _err);
-				return true;
-			case GL_INVALID_ENUM:
-				printf("GL_INVALID_ENUM: An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag. | %s -> %u \n",  _message, _err);
-				return true;
-			case GL_INVALID_VALUE:
-				printf("GL_INVALID_VALUE: A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag. | %s -> %u \n",  _message, _err);
-				return true;
-			case GL_INVALID_OPERATION:
-				printf("GL_INVALID_OPERATION: The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag. | %s -> %u \n",  _message, _err);
-				return true;
-			case GL_INVALID_FRAMEBUFFER_OPERATION:
-				printf("GL_INVALID_FRAMEBUFFER_OPERATION: The framebuffer object is not complete. | %s -> %u \n",  _message, _err);
-				return true;
-			case GL_OUT_OF_MEMORY:
-				printf("GL_OUT_OF_MEMORY: There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded. | %s -> %u \n",  _message, _err);
-				return true;
-			case GL_STACK_UNDERFLOW:
-				printf("GL_STACK_UNDERFLOW: An attempt has been made to perform an operation that would cause an internal stack to underflow. | %s -> %u \n",  _message, _err);
-				return true;
-			case GL_STACK_OVERFLOW:
-				printf("GL_STACK_OVERFLOW: An attempt has been made to perform an operation that would cause an internal stack to overflow. | %s -> %u \n",  _message, _err);
-				return true;
-			default:
-				printf("No description. | %s -> %u \n",  _message, _err);
-				return true;
-		}
-	}
-
-	return false;
-}
-
 void rde_rendering_set_rendering_configuration() {
 
 
