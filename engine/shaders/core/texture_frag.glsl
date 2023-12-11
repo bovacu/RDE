a65109d020228a4a437e6ffc597fa4cd3ca3bc06
Borja vazquez <boja.vazquez@byaliens.com>
2023-12-11 15:02:31 -0300

    Set correct naming conventions now it is finished

--- engine/shaders/core/texture_frag.glsl
+++ engine/shaders/core/texture_frag.glsl
@@ -4,64 +4,80 @@ header_2d_frag
 
 vec4 draw_nine_slice() {
 		// This is just to make the image scale correctly
-	vec2 prop = vec2(sub_texture_uv.z - sub_texture_uv.x, sub_texture_uv.w - sub_texture_uv.y);
+	vec2 _prop = vec2(sub_texture_uv.z - sub_texture_uv.x, sub_texture_uv.w - sub_texture_uv.y);
 
 	// Point of start if sub-texture
-	vec2 offset = vec2(sub_texture_uv.x, sub_texture_uv.y);
-	vec2 _uv = uv;
-    vec2 ratio = (texture_size.xy / size.xy);
-    vec2 border = vec2(nine_slice.x, nine_slice.z);
-    vec2 border_custom_prop = (border / size) * prop;
-    vec2 border_original_prop = (border / texture_size) * prop;
-	vec2 texture_coord = vec2(0);
-	vec2 atlas_offset = vec2(0);
+    vec2 _ratio = (texture_size.xy / size.xy);
+    vec2 _border = vec2(nine_slice.x, nine_slice.z);
+    vec2 _border_custom_prop = (_border / size) * _prop;
+    vec2 _border_original_prop = (_border / texture_size) * _prop;
+	vec2 _texture_coord = vec2(0);
+	vec2 _atlas_offset = vec2(0);
 
+	//		x padding min	x padding max
+	//       	   |			|
+	// 	  *------*------------*------*
+	// 	  |	  |			|	  |
+	// 	  |  L/T |	 M/T	|  R/T |
+	// 	  |	  |			|	  |
+	// 	--*------*------------*------*-- y padding max
+	// 	  |	  |			|	  |
+	// 	  |  L/M |	 M/M	|  R/M |
+	// 	  |	  |			|	  |
+	// 	--*------*------------*------*-- y padding min
+	// 	  |	  |			|	  |
+	// 	  |  L/D |	 M/D	|  R/D |
+	// 	  |	  |			|	  |
+	// 	  *------*------------*------* 
+
+	// --------------------- X Axis
 	// Left
-    if(_uv.x < offset.x + border_custom_prop.x) {
-        texture_coord.x = (_uv.x - offset.x) / ratio.x;
-		atlas_offset.x = sub_texture_uv.x;
+    if(uv.x < sub_texture_uv.x + _border_custom_prop.x) {
+        _texture_coord.x = (uv.x - sub_texture_uv.x) / _ratio.x;
+		_atlas_offset.x = sub_texture_uv.x;
     } 
 
 	// Right
-	else if(_uv.x > (sub_texture_uv.z - border_custom_prop.x)) {
-		texture_coord.x = (_uv.x - sub_texture_uv.z) / ratio.x;
-		atlas_offset.x = sub_texture_uv.z;
+	else if(uv.x > (sub_texture_uv.z - _border_custom_prop.x)) {
+		_texture_coord.x = (uv.x - sub_texture_uv.z) / _ratio.x;
+		_atlas_offset.x = sub_texture_uv.z;
 	}
 	
 	// Middle
 	else {
-		texture_coord.x = _uv.x + (border_original_prop.x - border_custom_prop.x);
-		if(texture_coord.x > sub_texture_uv.z - border_original_prop.x) {
-			texture_coord.x = sub_texture_uv.z - border_original_prop.x;
+		_texture_coord.x = uv.x + (_border_original_prop.x - _border_custom_prop.x);
+		if(_texture_coord.x > sub_texture_uv.z - _border_original_prop.x) {
+			_texture_coord.x = sub_texture_uv.z - _border_original_prop.x;
 		}
 	}
 	
+	// --------------------- Y Axis
 	// Down
-    if(_uv.y < offset.y + border_custom_prop.y) {
-		texture_coord.y = (_uv.y - offset.y) / ratio.y;
-		atlas_offset.y = sub_texture_uv.y;
+    if(uv.y < sub_texture_uv.y + _border_custom_prop.y) {
+		_texture_coord.y = (uv.y - sub_texture_uv.y) / _ratio.y;
+		_atlas_offset.y = sub_texture_uv.y;
     }
 	
 	// Up
-	else if(_uv.y > (sub_texture_uv.w - border_custom_prop.y)) {
-		texture_coord.y = (_uv.y - sub_texture_uv.w) / ratio.y;
-		atlas_offset.y = sub_texture_uv.w;
+	else if(uv.y > (sub_texture_uv.w - _border_custom_prop.y)) {
+		_texture_coord.y = (uv.y - sub_texture_uv.w) / _ratio.y;
+		_atlas_offset.y = sub_texture_uv.w;
 	}
 
 	// Middle
 	else {
-		texture_coord.y = _uv.y + border_original_prop.y - border_custom_prop.y;
-		if(texture_coord.y > sub_texture_uv.w - border_original_prop.y) {
-			texture_coord.y = sub_texture_uv.w - border_original_prop.y;
+		_texture_coord.y = uv.y + _border_original_prop.y - _border_custom_prop.y;
+		if(_texture_coord.y > sub_texture_uv.w - _border_original_prop.y) {
+			_texture_coord.y = sub_texture_uv.w - _border_original_prop.y;
 		}
 	}
 
-	vec4 _color = texture(tex, texture_coord + atlas_offset);
+	vec4 _color = texture(tex, _texture_coord + _atlas_offset);
 	if(_color.w < 0.05) {
 		discard;
 	}
 
-    return texture(tex, texture_coord + atlas_offset) * vec4(color.x / 255.f, color.y / 255.f, color.z / 255.f, color.w / 255.f);
+    return texture(tex, _texture_coord + _atlas_offset) * vec4(color.x / 255.f, color.y / 255.f, color.z / 255.f, color.w / 255.f);
 }
 
 vec4 draw_texture() {
