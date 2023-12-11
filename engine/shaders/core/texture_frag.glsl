758015d1bf08191ffc9ea7324bbf22839eee3982
Borja vazquez <boja.vazquez@byaliens.com>
2023-12-11 14:41:02 -0300

    Solved 9Slice on texture atlases

--- engine/shaders/core/texture_frag.glsl
+++ engine/shaders/core/texture_frag.glsl
@@ -11,42 +11,49 @@ void main() {
 	vec2 _uv = uv;
     vec2 ratio = (texture_size.xy / size.xy);
     vec2 border = vec2(nine_slice.x, nine_slice.z);
-    vec2 bDst = (border / size) * prop;
-	vec2 texture_coord = vec2(-1, -1);
+    vec2 border_custom_prop = (border / size) * prop;
+    vec2 border_original_prop = (border / texture_size) * prop;
+	vec2 texture_coord = vec2(0);
 	vec2 atlas_offset = vec2(0);
 
 	// Left
-    if(_uv.x < offset.x + bDst.x) {
+    if(_uv.x < offset.x + border_custom_prop.x) {
         texture_coord.x = (_uv.x - offset.x) / ratio.x;
 		atlas_offset.x = sub_texture_uv.x;
     } 
 
 	// Right
-	else if(_uv.x > (sub_texture_uv.z - bDst.x)) {
+	else if(_uv.x > (sub_texture_uv.z - border_custom_prop.x)) {
 		texture_coord.x = (_uv.x - sub_texture_uv.z) / ratio.x;
 		atlas_offset.x = sub_texture_uv.z;
 	}
 	
 	// Middle
 	else {
-		texture_coord.x = _uv.x + (((border / texture_size) * prop).x - bDst.x);
+		texture_coord.x = _uv.x + (border_original_prop.x - border_custom_prop.x);
+		if(texture_coord.x > sub_texture_uv.z - border_original_prop.x) {
+			texture_coord.x = sub_texture_uv.z - border_original_prop.x;
+		}
 	}
 	
 	// Down
-    if(_uv.y < offset.y + bDst.y) {
+    if(_uv.y < offset.y + border_custom_prop.y) {
 		texture_coord.y = (_uv.y - offset.y) / ratio.y;
 		atlas_offset.y = sub_texture_uv.y;
     }
 	
 	// Up
-	else if(_uv.y > (sub_texture_uv.w - bDst.y)) {
+	else if(_uv.y > (sub_texture_uv.w - border_custom_prop.y)) {
 		texture_coord.y = (_uv.y - sub_texture_uv.w) / ratio.y;
 		atlas_offset.y = sub_texture_uv.w;
 	}
 
 	// Middle
 	else {
-		texture_coord.y = _uv.y + (((border / texture_size) * prop).y - bDst.y);
+		texture_coord.y = _uv.y + border_original_prop.y - border_custom_prop.y;
+		if(texture_coord.y > sub_texture_uv.w - border_original_prop.y) {
+			texture_coord.y = sub_texture_uv.w - border_original_prop.y;
+		}
 	}
 
 	vec4 _color = texture(tex, texture_coord + atlas_offset);
