e163e2f403928fd067d4c3756ff7b8f38b511f70
Borja vazquez <boja.vazquez@byaliens.com>
2023-12-11 10:18:36 -0300

    Corners are finally working correctly

--- engine/shaders/core/texture_frag.glsl
+++ engine/shaders/core/texture_frag.glsl
@@ -9,42 +9,52 @@ void main() {
 	// Point of start if sub-texture
 	vec2 offset = vec2(sub_texture_uv.x, sub_texture_uv.y);
 	vec2 _uv = uv;
-    vec2 ratio = (texture_size.xy / size.xy) * prop;
+    vec2 ratio = (texture_size.xy / size.xy);
     vec2 border = vec2(nine_slice.x, nine_slice.z);
-    vec2 bSrc = border / texture_size;
-    vec2 bDst = border / size;
+    vec2 bDst = (border / size) * prop;
+	vec2 texture_coord = vec2(-1, -1);
+	vec2 atlas_offset = vec2(0);
 
 	// Left
     if(_uv.x < offset.x + bDst.x) {
-        _uv.x = (_uv.x - offset.x) / ratio.x;
+        texture_coord.x = (_uv.x - offset.x) / ratio.x;
+		atlas_offset.x = sub_texture_uv.x;
     } 
 
 	// Right
 	else if(_uv.x > (sub_texture_uv.z - bDst.x)) {
-        _uv.x = 1.0 - (sub_texture_uv.z - _uv.x) / ratio.x;
-    } 
+		texture_coord.x = (_uv.x - sub_texture_uv.z) / ratio.x;
+		atlas_offset.x = sub_texture_uv.z;
+	}
 	
 	// Middle
-	else {
-        float t = (border.x * (texture_size.x - size.x)) / (texture_size.x * (2.0 * border.x - size.x));
-        _uv.x = _uv.x * (1.0 - t * 2.0) + t;
-    }
+	// else {
+	//        float t = (border.x * (texture_size.x - size.x)) / (texture_size.x * (2.0 * border.x - size.x));
+	//        texture_coord.x = _uv.x * (1.0 - t * 2.0) + t;
+	//    }
 	
 	// Down
-    if(_uv.y < bDst.y + offset.y) {
-		_uv.y = (_uv.y - offset.y) / ratio.y;
-    } 
+    if(_uv.y < offset.y + bDst.y) {
+		texture_coord.y = (_uv.y - offset.y) / ratio.y;
+		atlas_offset.y = sub_texture_uv.y;
+    }
 	
 	// Up
 	else if(_uv.y > (sub_texture_uv.w - bDst.y)) {
-        _uv.y = 1.0 - (sub_texture_uv.w - _uv.y) / ratio.y;
-    } 
+		texture_coord.y = (_uv.y - sub_texture_uv.w) / ratio.y;
+		atlas_offset.y = sub_texture_uv.w;
+	}
 
 	// Middle
-	else {
-        float t = (border.y * (texture_size.y - size.y)) / (texture_size.y * (2.0 * border.y - size.y));
-        _uv.y = _uv.y * (1.0 - t * 2.0) + t;
-    }
+	// else {
+	//        float t = (border.y * (texture_size.y - size.y)) / (texture_size.y * (2.0 * border.y - size.y));
+	//        texture_coord.y = _uv.y * (1.0 - t * 2.0) + t;
+	//    }
 
-    out_color = texture(tex, (prop * _uv + offset));
+	// if(texture_coord.x == -1 || texture_coord.y == -1) {
+	// 	out_color = vec4(1, 0, 0, 1);
+	// 	return;
+	// }
+
+    out_color = texture(tex, texture_coord + atlas_offset);
 }
