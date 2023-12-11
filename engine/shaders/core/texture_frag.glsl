1211bc4341ba1babe1c26f89cf245b1917fa78ea
Bovacu <vazquezborja@hotmail.es>
2023-12-10 23:13:58 -0300

    Improve a little

--- engine/shaders/core/texture_frag.glsl
+++ engine/shaders/core/texture_frag.glsl
@@ -2,33 +2,11 @@
 
 header_2d_frag
 
-vec2 uv9slice(vec2 uv, vec2 s, vec4 b) {
-    vec2 t = clamp((s * uv - b.xy) / (s - b.xy - b.zw), 0., 1.);
-	return mix(uv * s, 1. - s * (1. - uv), t);
-}
-
-vec4 draw_nine_slice() {
-	vec2 _s = size.xy / texture_size * vec2(0.25, 1.0 - 0.75);
-    vec4 _b = nine_slice / texture_size.xxyy;
-    vec2 _uv = uv9slice(uv, _s, _b);
-    vec3 _col = vec3(texture(tex, (vec2(0.25, 1.0 - 0.75) * _uv + vec2(0.0, 0.75))).x);
-	return vec4(_col, 1.0);
-}
-
-vec4 draw_texture() {
-	return texture(tex, uv) * vec4(color.x / 255.f, color.y / 255.f, color.z / 255.f, color.w / 255.f);
-}
-
-// void main(void) {
-// 	if(any(greaterThan(nine_slice, vec4(0)))) {
-// 		out_color = draw_nine_slice();
-// 	} else {
-// 		out_color = draw_texture();
-// 	}
-// }
-
 void main() {
+	// This is just to make the image scale correctly
 	vec2 prop = vec2(sub_texture_uv.z - sub_texture_uv.x, sub_texture_uv.w - sub_texture_uv.y);
+
+	// Point of start if sub-texture
 	vec2 offset = vec2(sub_texture_uv.x, sub_texture_uv.y);
 	vec2 _uv = uv;
     vec2 ratio = (texture_size.xy / size.xy) * prop;
@@ -37,13 +15,13 @@ void main() {
     vec2 bDst = border / size;
 
 	// Left
-    if(_uv.x < bDst.x) {
-        _uv.x = _uv.x / ratio.x;
+    if(_uv.x < offset.x + bDst.x) {
+        _uv.x = (_uv.x + offset.x) / ratio.x;
     } 
 
 	// Right
 	else if(_uv.x > (sub_texture_uv.z - bDst.x)) {
-        _uv.x = (sub_texture_uv.z - _uv.x) / ratio.x;
+        _uv.x = 1.0 - (sub_texture_uv.z - _uv.x) / ratio.x;
     } 
 	
 	// Middle
@@ -58,8 +36,8 @@ void main() {
     } 
 	
 	// Up
-	else if(_uv.y > (1.0 - bDst.y)) {
-        _uv.y = 1.0 - (1.0 - _uv.y) / ratio.y;
+	else if(_uv.y > (sub_texture_uv.w - bDst.y)) {
+        _uv.y = 1.0 - (sub_texture_uv.w - _uv.y) / ratio.y;
     } 
 
 	// Middle
