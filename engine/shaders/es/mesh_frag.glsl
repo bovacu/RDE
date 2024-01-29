b451e24ef03622e48625ebf6fb206f0296b7316e
Bovacu <vazquezborja@hotmail.es>
2024-01-29 01:26:55 -0300

    Fix shader for improving A LOT mobile performance

--- engine/shaders/es/mesh_frag.glsl
+++ engine/shaders/es/mesh_frag.glsl
@@ -73,25 +73,19 @@ vec3 directional_light_calc() {
 	vec3 _light_dir = normalize(-directional_light.direction);
 	vec3 _norm = normalize(normal);
 
-	if(material.Ka != vec3(0, 0, 0)) {
-		vec3 _ambient = material.Ka * directional_light.ambient_color * texture(tex_kd, text_coord).rgb;
-		_final_light += _ambient;
-	}
+	vec3 _ambient = material.Ka * directional_light.ambient_color * texture(tex_kd, text_coord).rgb;
+	_final_light += _ambient;
 
 
-	if(material.Kd != vec3(0, 0, 0)) {
-		float _diff = max(dot(_norm, _light_dir), 0.0);
-		vec3 _diffuse = material.Kd * directional_light.diffuse_color * _diff * texture(tex_kd, text_coord).rgb;
-		_final_light += _diffuse;
-	}
+	float _diff = max(dot(_norm, _light_dir), 0.0);
+	vec3 _diffuse = material.Kd * directional_light.diffuse_color * _diff * texture(tex_kd, text_coord).rgb;
+	_final_light += _diffuse;
 
-	if(material.Ks != vec3(0, 0, 0)) {
-		vec3 _view_dir = normalize(camera_pos + frag_pos);
-		vec3 _reflect_dir = reflect(-_light_dir, _norm);
-		float _spec = pow(max(dot(_view_dir, _reflect_dir), 0.0), material.shininess);
-		vec3 _specular = material.Ks * directional_light.specular_color * _spec * texture(tex_ks, text_coord).rgb;
-		_final_light += _specular;
-	}
+	vec3 _view_dir = normalize(camera_pos + frag_pos);
+	vec3 _reflect_dir = reflect(-_light_dir, _norm);
+	float _spec = pow(max(dot(_view_dir, _reflect_dir), 0.0), material.shininess);
+	vec3 _specular = material.Ks * directional_light.specular_color * _spec * texture(tex_ks, text_coord).rgb;
+	_final_light += _specular;
 
 	return _final_light;
 }
@@ -107,24 +101,18 @@ vec3 point_light_calc(int _i) {
 	vec3 _light_dir = normalize(_light.position - frag_pos);
 	vec3 _final_light = vec3(0, 0, 0);
 
-	if(material.Ka != vec3(0, 0, 0)) {
-		vec3 _ambient = material.Ka * _light.ambient_color * texture(tex_kd, text_coord).rgb;
-		_final_light += _ambient * _attenuation;
-	}
+	vec3 _ambient = material.Ka * _light.ambient_color * texture(tex_kd, text_coord).rgb;
+	_final_light += _ambient * _attenuation;
 
-	if(material.Kd != vec3(0, 0, 0)) {
-		float _diff = max(dot(_norm, _light_dir), 0.0);
-		vec3 _diffuse = material.Kd * _light.diffuse_color * _diff * texture(tex_kd, text_coord).rgb;
-		_final_light += _diffuse * _attenuation;
-	}
+	float _diff = max(dot(_norm, _light_dir), 0.0);
+	vec3 _diffuse = material.Kd * _light.diffuse_color * _diff * texture(tex_kd, text_coord).rgb;
+	_final_light += _diffuse * _attenuation;
 
-	if(material.Ks != vec3(0, 0, 0)) {
-		vec3 _view_dir = normalize(camera_pos + frag_pos);
-		vec3 _reflect_dir = reflect(-_light_dir, _norm);
-		float _spec = pow(max(dot(_view_dir, _reflect_dir), 0.0), material.shininess);
-		vec3 _specular = material.Ks * _light.specular_color * _spec * texture(tex_ks, text_coord).rgb;
-		_final_light += _specular * _attenuation;
-	}
+	vec3 _view_dir = normalize(camera_pos + frag_pos);
+	vec3 _reflect_dir = reflect(-_light_dir, _norm);
+	float _spec = pow(max(dot(_view_dir, _reflect_dir), 0.0), material.shininess);
+	vec3 _specular = material.Ks * _light.specular_color * _spec * texture(tex_ks, text_coord).rgb;
+	_final_light += _specular * _attenuation;
 
 	return _final_light;
 }
@@ -143,39 +131,35 @@ vec3 spot_light_calc(int _i) {
 	float _attenuation = 1.0 / (_light.constant + _light.linear * _distance + _light.quadratic * (_distance * _distance));
 	vec3 _final_light = vec3(0, 0, 0);
 
-	if(material.Ka != vec3(0, 0, 0)) {
-		vec3 _ambient = material.Ka * _light.ambient_color * texture(tex_kd, text_coord).rgb;
-		_final_light += _ambient * _attenuation;
-	}
+	vec3 _ambient = material.Ka * _light.ambient_color * texture(tex_kd, text_coord).rgb;
+	_final_light += _ambient * _attenuation;
 
-	if(material.Kd != vec3(0, 0, 0)) {
-		vec3 _norm = normalize(normal);
-		float _diff = max(dot(_norm, _light_dir), 0.0);
-		vec3 _diffuse = material.Kd * _light.diffuse_color * _diff * texture(tex_kd, text_coord).rgb;
-		_final_light += _diffuse * _attenuation * _intensity;
-	}
+	vec3 _norm = normalize(normal);
+	float _diff = max(dot(_norm, _light_dir), 0.0);
+	vec3 _diffuse = material.Kd * _light.diffuse_color * _diff * texture(tex_kd, text_coord).rgb;
+	_final_light += _diffuse * _attenuation * _intensity;
 
 	return _final_light;
 }
 #endif
 
 void normal_rendering() {
-	if(texture(tex_kd, text_coord).a < 0.05) discard;
+// 	if(texture(tex_kd, text_coord).a < 0.05) discard;
 	vec3 _final_light = vec3(0.0);
 
 	_final_light += directional_light_calc();
 
-#if RDE_MAX_POINT_LIGHTS
-	for (int _i = 0; _i < RDE_MAX_POINT_LIGHTS; _i++) {
-		_final_light += point_light_calc(_i);
-	}
-#endif
-
-#if RDE_MAX_SPOT_LIGHTS
-	for (int _i = 0; _i < RDE_MAX_SPOT_LIGHTS; _i++) {
-		_final_light += spot_light_calc(_i);
-	}
-#endif
+// #if RDE_MAX_POINT_LIGHTS
+// 	for (int _i = 0; _i < RDE_MAX_POINT_LIGHTS; _i++) {
+// 		_final_light += point_light_calc(_i);
+// 	}
+// #endif
+// 
+// #if RDE_MAX_SPOT_LIGHTS
+// 	for (int _i = 0; _i < RDE_MAX_SPOT_LIGHTS; _i++) {
+// 		_final_light += spot_light_calc(_i);
+// 	}
+// #endif
 
 	color_out = vec4(_final_light, 1.0);
 }
@@ -185,9 +169,10 @@ void render_texture_rendering() {
 }
 
 void main(){
-	if(material.using_render_texture == 0) {
-		normal_rendering();
-	} else {
-		render_texture_rendering();
-	}
+	normal_rendering();
+// 	if(material.using_render_texture == 0) {
+// 		normal_rendering();
+// 	} else {
+// 		render_texture_rendering();
+// 	}
 }
